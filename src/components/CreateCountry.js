fb9fbcd8e02a06227b4e26573e702743166c5ff2
uedi <uedi@users.noreply.github.com>
2021-01-14 09:14:13 +0200

    use reducer to save country

--- src/components/CreateCountry.js
+++ src/components/CreateCountry.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import CreateCountryForm from './CreateCountryForm'
 import countriesService from '../services/countries'
+import { useHistory } from 'react-router-dom'
+import { addCountry } from '../reducers/countryReducer'
 
 const CreateCountry = () => {
-    
+    const history = useHistory()
+    const dispatch = useDispatch()
+
     const handleCreateCountry = (data) => {
         countriesService.create(data)
         .then(response => {
-            console.log('created country:', response)
+            dispatch(addCountry(response))
+            history.push('/countries')
         })
         .catch(error => {
             console.log('handleCreateCountry error', error)
--- src/reducers/countryReducer.js
+++ src/reducers/countryReducer.js
@@ -2,6 +2,8 @@ const countryReducer = (state = null, action) => {
     switch(action.type) {
         case 'SET_COUNTRIES':
             return action.data
+        case 'ADD_COUNTRY':
+            return [...state, action.data]
         default:
             return state
     }
@@ -11,6 +13,13 @@ export const setCountries = (countries) => {
     return {
         type: 'SET_COUNTRIES',
         data: countries
+    }
+}
+
+export const addCountry = (country) => {
+    return {
+        type: 'ADD_COUNTRY',
+        data: country
     }
 }
 
